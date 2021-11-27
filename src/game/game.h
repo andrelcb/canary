4802f1bb84592790f4cc8502c5fa7cc10cd65110
Beats-Dh <daniel15042014@hotmail.com>
2021-11-27 16:37:36 -0400

    Keep monsters and npcs in hash map for faster access
    Co-Authored-By: Jakub <38609240+SaiyansKing@users.noreply.github.com>

--- src/game/game.h
+++ src/game/game.h
@@ -452,18 +452,10 @@ class Game {
 
 		void sendOfflineTrainingDialog(Player* player);
 
-		const std::map<uint16_t, std::map<uint8_t, uint64_t>> &getItemsPrice() const {
-			return itemsPriceMap;
-		}
-		const phmap::flat_hash_map<uint32_t, Player*> &getPlayers() const {
-			return players;
-		}
-		const std::map<uint32_t, Monster*> &getMonsters() const {
-			return monsters;
-		}
-		const std::map<uint32_t, Npc*> &getNpcs() const {
-			return npcs;
-		}
+		const std::map<uint16_t, std::map<uint8_t, uint64_t>>& getItemsPrice() const { return itemsPriceMap; }
+		const phmap::flat_hash_map<uint32_t, Player*>& getPlayers() const { return players; }
+		const phmap::flat_hash_map<uint32_t, Monster*>& getMonsters() const { return monsters; }
+		const phmap::flat_hash_map<uint32_t, Npc*>& getNpcs() const { return npcs; }
 
 		const std::vector<ItemClassification*> &getItemsClassifications() const {
 			return itemsClassifications;
@@ -582,6 +574,8 @@ class Game {
 
 		phmap::flat_hash_map<uint32_t, Player*> players;
 		phmap::flat_hash_map<std::string, Player*> mappedPlayerNames;
+		phmap::flat_hash_map<uint32_t, Npc*> npcs;
+		phmap::flat_hash_map<uint32_t, Monster*> monsters;
 		phmap::flat_hash_map<uint32_t, Guild*> guilds;
 		phmap::flat_hash_map<uint16_t, Item*> uniqueItems;
 		std::map<uint32_t, uint32_t> stages;
@@ -609,8 +603,6 @@ class Game {
 
 		WildcardTreeNode wildcardTree { false };
 
-		std::map<uint32_t, Npc*> npcs;
-		std::map<uint32_t, Monster*> monsters;
 		std::vector<uint32_t> forgeableMonsters;
 
 		std::map<uint32_t, TeamFinder*> teamFinderMap; // [leaderGUID] = TeamFinder*
