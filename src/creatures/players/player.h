70fb212755a7d1b7f32ff2a72d34935091ff71f7
Beats <daniel15042014@hotmail.com>
2022-10-22 23:47:08 -0300

    Adjustment of Damage and Healing Scaling per Level

--- src/creatures/players/player.h
+++ src/creatures/players/player.h
@@ -735,6 +735,11 @@ class Player final : public Creature, public Cylinder {
 		void setFightMode(FightMode_t mode) {
 			fightMode = mode;
 		}
+
+		uint8_t getFightMode() const {
+			return fightMode;
+		}
+
 		void setSecureMode(bool mode) {
 			secureMode = mode;
 		}
@@ -814,6 +819,7 @@ class Player final : public Creature, public Cylinder {
 		int32_t getArmor() const override;
 		int32_t getDefense() const override;
 		float getAttackFactor() const override;
+		float getDistFactor() const override;
 		float getDefenseFactor() const override;
 
 		void addInFightTicks(bool pzlock = false);
@@ -2289,6 +2295,8 @@ class Player final : public Creature, public Cylinder {
 				client->sendInventoryImbuements(items);
 			}
 		}
+
+		int32_t getBaseAttack(uint32_t level) const;
 
 	private:
 		static uint32_t playerFirstID;
